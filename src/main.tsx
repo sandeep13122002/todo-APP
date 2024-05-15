e94ffdbd8463cb0780005e15f6feeb4094211402
sandeep <sandykumaryadav70@gmail.com>
2024-05-15 22:52:31 +0530

    context api implemented

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { TodosProvider } from './store/todo.tsx'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <TodosProvider>
     <App />
+    </TodosProvider>
+   
   </React.StrictMode>,
 )
--- src/store/todo.tsx
+++ src/store/todo.tsx
@@ -1,6 +1,35 @@
-import {createContext} from 'react';
-
-export const todosContext=createContext(null)
+import {ReactNode, createContext, useState} from 'react';
 
 
-export const TodosProvider=()
+export type todosProviderProps={
+    children:ReactNode;
+}
+
+export type Todo={
+    id:string;
+    task:string;
+    completed:boolean;
+    createdAt:Date;
+}
+export type TodosContext={
+    todos:Todo[];
+     handelAddToDo:(task:string)=>void;
+}
+
+
+export const todosContext=createContext<TodosContext|null>(null)
+
+
+export const TodosProvider=({children}:todosProviderProps)=>{
+     
+    const [todos,setTodos]=useState<Todo[]>([])
+      const handelAddToDo=(task:string)=>{
+
+      }
+
+    return <todosContext.Provider  value={{todos,handelAddToDo}}>
+        {children}
+    </todosContext.Provider>
+          
+    
+}
